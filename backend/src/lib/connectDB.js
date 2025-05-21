142f7caa17f24ac036935951e7f9db20b872e4b6
Krish Srivastava <krish.2022ug3018@iiitranchi.ac.in>
2025-05-21 16:25:34 +0530

    feat: update package.json scripts and modify MongoDB connection logic

--- backend/src/lib/connectDB.js
+++ backend/src/lib/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/louder")
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
